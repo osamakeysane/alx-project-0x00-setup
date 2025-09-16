833f45c354cd3dbd2e37b5900f96cedc3bce51e1
mohamed keytsane <osamakeysane20@gmail.com>
2025-09-16 12:00:12 +0300

    imported Card insidr the landing page

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import Card from "@/components/Card"; // adjust path if needed
 
 const Landing: React.FC = () => {
   return (
     <div>
       <h1 className="text-xl font-extralight">Landing Page</h1>
+      <div className="p-10 flex justify-center">
+        <Card />
+      </div>
     </div>
   );
 };
