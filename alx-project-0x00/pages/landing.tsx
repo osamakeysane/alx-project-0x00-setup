18bd86ab58fee1640b251ae2cae55b9000b7fe0f
mohamed keytsane <osamakeysane20@gmail.com>
2025-09-16 12:20:25 +0300

    duplicated Card multiple times

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -3,11 +3,11 @@ import Card from "@/components/Card"; // adjust path if needed
 
 const Landing: React.FC = () => {
   return (
-    <div>
+    <div className="p-10">
       <h1 className="text-xl font-extralight">Landing Page</h1>
-      <div className="p-10 flex justify-center">
-        <Card />
-      </div>
+      <Card />
+      <Card />
+      <Card />
     </div>
   );
 };
